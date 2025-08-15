77837b57cee2aaf508ed0f6138bb762dff1f05fb
Anashya <anashyajayarathna@gmail.com>
2025-08-15 21:54:36 +0530

    Create arraystack.java

---
+++ arraystack.java
@@ -0,0 +1,49 @@
+public class ArrayStack {
+    private int[] stack;
+    private int top;
+    private int capacity;
+
+    public ArrayStack(int size) {
+        stack = new int[size];
+        capacity = size;
+        top = -1;
+    }
+
+    public void push(int item) {
+        if (isFull()) {
+            System.out.println("Stack is full");
+            return;
+        }
+        stack[++top] = item;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return stack[top--];
+    }
+
+    public int peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        }
+        return -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public void printStack() {
+        for (int i = 0; i <= top; i++) {
+            System.out.print(stack[i] + " ");
+        }
+        System.out.println();
+    }
+}
